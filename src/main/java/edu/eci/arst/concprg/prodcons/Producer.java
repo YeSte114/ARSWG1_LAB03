541ad18143fc1e1284ba7e70a0eaea793886d243
YeSte114 <46783436+YeSte114@users.noreply.github.com>
2022-08-25 21:32:08 -0500

    parte 3

--- src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
+++ src/main/java/edu/eci/arst/concprg/prodcons/Producer.java
@@ -31,16 +31,18 @@ public class Producer extends Thread {
     @Override
     public void run() {
         while (true) {
+            if (queue.size()+1<=this.stockLimit){
+                dataSeed = dataSeed + rand.nextInt(100);
+                System.out.println("Producer added " + dataSeed);
+                queue.add(dataSeed);
+            }
 
-            dataSeed = dataSeed + rand.nextInt(100);
-            System.out.println("Producer added " + dataSeed);
-            queue.add(dataSeed);
-            
+            /*
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(Producer.class.getName()).log(Level.SEVERE, null, ex);
-            }
+            }*/
 
         }
     }
